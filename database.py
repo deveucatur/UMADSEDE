33efc9914e2ccb5c82b86a40f60f960b6bfebad1
deveucatur <117214850+deveucatur@users.noreply.github.com>
2024-10-30 21:33:57 -0400

    Update database.py

--- database.py
+++ database.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, Column, Integer, String, Date, Boolean, ForeignKey, UniqueConstraint
+from sqlalchemy import create_engine, Column, Integer, String, Date, Boolean, Text, ForeignKey, UniqueConstraint
 from sqlalchemy.orm import declarative_base, sessionmaker
 import datetime
 
@@ -6,15 +6,19 @@ engine = create_engine('sqlite:///adolescentes.db')
 Base = declarative_base()
 Session = sessionmaker(bind=engine)
 session = Session()
-class Adolescente(Base):
-    __tablename__ = 'adolescentes'
+from database import Base
+
+class Pessoa(Base):
+    __tablename__ = 'pessoas'
     id = Column(Integer, primary_key=True)
-    nome = Column(String, unique=True)  # Nome único
+    nome = Column(String)
     data_nascimento = Column(Date)
     telefone = Column(String)
     batizado_aguas = Column(Boolean)
     batizado_espirito = Column(Boolean)
     status = Column(String)
+    tipo = Column(String)  # 'Jovem' ou 'Adolescente'
+    observacao = Column(Text)
 
 
 class Evento(Base):
@@ -22,22 +26,29 @@ class Evento(Base):
     id = Column(Integer, primary_key=True)
     nome = Column(String)
     data = Column(Date)
-    encerrado = Column(Boolean, default=False) 
+    encerrado = Column(Boolean, default=False)
+    tipo = Column(String)  # 'Jovens', 'Adolescentes' ou 'Ambos'
     
 class Visitante(Base):
     __tablename__ = 'visitantes'
     id = Column(Integer, primary_key=True)
     nome = Column(String)
     telefone = Column(String)
-    convidado_por = Column(Integer, ForeignKey('adolescentes.id'))
-    evento_id = Column(Integer, ForeignKey('eventos.id'))  # Certifique-se de que este campo está presente
+    convidado_por = Column(Integer, ForeignKey('pessoas.id'))
+    evento_id = Column(Integer, ForeignKey('eventos.id'))
+
+    convidado = relationship('Pessoa', foreign_keys=[convidado_por])
+    evento = relationship('Evento')
 
 
 class Presenca(Base):
     __tablename__ = 'presencas'
     id = Column(Integer, primary_key=True)
-    adolescente_id = Column(Integer, ForeignKey('adolescentes.id'))
+    pessoa_id = Column(Integer, ForeignKey('pessoas.id'))
     evento_id = Column(Integer, ForeignKey('eventos.id'))
-    presente = Column(Boolean)  # Certifique-se de que este campo está presente
+    presente = Column(Boolean)
+
+    pessoa = relationship('Pessoa')
+    evento = relationship('Evento')
 
 Base.metadata.create_all(engine)
