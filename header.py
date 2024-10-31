ae8693ca29c2e383e8c300572e6faba4d95f944c
deveucatur <117214850+deveucatur@users.noreply.github.com>
2024-10-31 00:03:48 -0400

    Update header.py

--- header.py
+++ header.py
@@ -27,7 +27,7 @@ def header_css():
         color: #fff;
         padding: 10px 20px;
         width: 100%;
-        height: 70px;
+        height: 55px;
         border-bottom-left-radius: 15px;
         border-bottom-right-radius: 15px;
     }}
